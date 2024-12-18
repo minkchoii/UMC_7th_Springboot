9c1738ae631bf003225148f210e5e55e87b40dd1
minkchoii <your_email@example.com>
2024-12-18 10:35:29 +0900

    Feat : review repository 페이지이용해 리뷰찾

--- src/main/java/spring/umc7th/repository/ReviewRepository.java
+++ src/main/java/spring/umc7th/repository/ReviewRepository.java
@@ -1,7 +1,12 @@
 package spring.umc7th.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.umc7th.domain.Review;
+import spring.umc7th.domain.Store;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findAllByStore(Store store, PageRequest pageRequest);
 }
