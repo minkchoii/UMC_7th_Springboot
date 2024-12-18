b4eb7c974ac365b01354b1b209636d8300c086d1
minkchoii <your_email@example.com>
2024-12-18 10:35:28 +0900

    Feat : create review method

--- src/main/java/spring/umc7th/service/storeService/StoreCommandServiceImpl.java
+++ src/main/java/spring/umc7th/service/storeService/StoreCommandServiceImpl.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.umc7th.converter.StoreConverter;
 import spring.umc7th.domain.Region;
+import spring.umc7th.domain.Review;
 import spring.umc7th.domain.Store;
+import spring.umc7th.repository.MemberRepository;
 import spring.umc7th.repository.RegionRepository;
+import spring.umc7th.repository.ReviewRepository;
 import spring.umc7th.repository.storeRepository.StoreRepository;
 import spring.umc7th.web.dto.StoreRequestDTO;
+import spring.umc7th.web.dto.StoreRequestDTO.ReviewDTO;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +20,8 @@ public class StoreCommandServiceImpl implements StoreCommandService {
 
     private final StoreRepository storeRepository;
     private final RegionRepository regionRepository;
+    private final MemberRepository memberRepository;
+    private final ReviewRepository reviewRepository;
 
     @Override
     @Transactional
@@ -25,5 +31,15 @@ public class StoreCommandServiceImpl implements StoreCommandService {
         Store store = StoreConverter.toStore(request, region);
 
         return storeRepository.save(store);
+    }
+
+    @Override
+    public Review createReview(Long memberId, Long storeId, ReviewDTO request) {
+        Review review = StoreConverter.toReview(request);
+
+        review.setMember(memberRepository.findById(memberId).get());
+        review.setStore(storeRepository.findById(storeId).get());
+
+        return reviewRepository.save(review);
     }
 }
