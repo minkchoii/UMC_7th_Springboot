b3125995df9ab244912ad97858a82714e6325ef1
minkchoii <your_email@example.com>
2024-12-18 10:35:27 +0900

    ✨  Feat: 엔티티 초기설정

--- src/main/java/spring/umc7th/Umc7thApplication.java
+++ src/main/java/spring/umc7th/Umc7thApplication.java
@@ -2,8 +2,10 @@ package spring.umc7th;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Umc7thApplication {
 
 	public static void main(String[] args) {
