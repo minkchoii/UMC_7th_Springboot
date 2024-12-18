1bd7c801c20dd112e136a401090bd8b29e9e443a
minkchoii <your_email@example.com>
2024-12-18 10:35:29 +0900

    Feat : review preview dto in member response

--- src/main/java/spring/umc7th/web/dto/MemberResponseDTO.java
+++ src/main/java/spring/umc7th/web/dto/MemberResponseDTO.java
@@ -1,6 +1,8 @@
 package spring.umc7th.web.dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,5 +17,29 @@ public class MemberResponseDTO {
     public static class JoinResultDTO{
         Long memberId;
         LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewListDTO{
+        List<MemberResponseDTO.ReviewPreViewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewDTO{
+        String ownerNickname;
+        Float score;
+        String body;
+        LocalDate createdAt;
     }
 }
