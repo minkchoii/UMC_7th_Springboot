69166e7f7d3c93a0c2c7f03962542cd9544ea960
minkchoii <your_email@example.com>
2024-12-18 10:35:28 +0900

    Feat : store converter

--- src/main/java/spring/umc7th/converter/StoreConverter.java
+++ src/main/java/spring/umc7th/converter/StoreConverter.java
@@ -2,6 +2,7 @@ package spring.umc7th.converter;
 
 import java.time.LocalDateTime;
 import spring.umc7th.domain.Region;
+import spring.umc7th.domain.Review;
 import spring.umc7th.domain.Store;
 import spring.umc7th.web.dto.StoreRequestDTO;
 import spring.umc7th.web.dto.StoreResponseDTO;
@@ -19,6 +20,21 @@ public class StoreConverter {
     public static StoreResponseDTO.CreateStoreResultDTO toCreateStoreResultDTO(Store store) {
         return StoreResponseDTO.CreateStoreResultDTO.builder()
                 .storeId(store.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static Review toReview(StoreRequestDTO.ReviewDTO request){
+        return Review.builder()
+                .title(request.getTitle())
+                .score(request.getScore())
+                .body(request.getBody())
+                .build();
+    }
+
+    public static StoreResponseDTO.CreateReviewResultDTO toCreateReviewResultDTO(Review review){
+        return StoreResponseDTO.CreateReviewResultDTO.builder()
+                .reviewId(review.getId())
                 .createdAt(LocalDateTime.now())
                 .build();
     }
