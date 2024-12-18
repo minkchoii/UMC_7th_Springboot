9f74348876798cd3a4e77f58776fefd6f6f05286
minkchoii <your_email@example.com>
2024-12-18 10:35:27 +0900

    Begin a project

---
+++ src/main/java/spring/umc7th/Umc7thApplication.java
@@ -0,0 +1,13 @@
+package spring.umc7th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc7thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc7thApplication.class, args);
+	}
+
+}
