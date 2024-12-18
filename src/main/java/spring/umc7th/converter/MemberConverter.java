683414dd154f4e54cd178ffed18502c28e87fd46
minkchoii <your_email@example.com>
2024-12-18 10:35:29 +0900

    Feat : review preview dto 변환 메서드 in member converter

--- src/main/java/spring/umc7th/converter/MemberConverter.java
+++ src/main/java/spring/umc7th/converter/MemberConverter.java
@@ -2,10 +2,15 @@ package spring.umc7th.converter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Page;
 import spring.umc7th.domain.Member;
+import spring.umc7th.domain.Review;
 import spring.umc7th.domain.enums.Gender;
 import spring.umc7th.web.dto.MemberRequestDTO;
 import spring.umc7th.web.dto.MemberResponseDTO;
+import spring.umc7th.web.dto.MemberResponseDTO.ReviewPreViewDTO;
 
 public class MemberConverter {
 
@@ -31,6 +36,29 @@ public class MemberConverter {
                 .gender(gender)
                 .name(request.getName())
                 .memberPreferList(new ArrayList<>())
+                .build();
+    }
+
+    public static MemberResponseDTO.ReviewPreViewDTO toReviewPreViewDTO(Review review){
+        return MemberResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getMember().getName())
+                .score(review.getScore())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getBody())
+                .build();
+    }
+
+    public static MemberResponseDTO.ReviewPreViewListDTO toReviewPreViewListDTO(Page<Review> reviewList){
+        List<ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(MemberConverter::toReviewPreViewDTO).collect(Collectors.toList());
+
+        return MemberResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
                 .build();
     }
 }
