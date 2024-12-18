817c268365d746e2d8a369730400e8659793fb11
minkchoii <your_email@example.com>
2024-12-18 10:35:29 +0900

    Feat : review preview list dto

--- src/main/java/spring/umc7th/web/dto/StoreResponseDTO.java
+++ src/main/java/spring/umc7th/web/dto/StoreResponseDTO.java
@@ -1,6 +1,8 @@
 package spring.umc7th.web.dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -42,5 +44,29 @@ public class StoreResponseDTO {
     public static class CreateMemberMissionResultDTO {
         Long memberMissionId;
         LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewListDTO{
+        List<ReviewPreViewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewDTO{
+        String ownerNickname;
+        Float score;
+        String body;
+        LocalDate createdAt;
     }
 }
