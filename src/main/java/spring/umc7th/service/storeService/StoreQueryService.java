791b782f2c3abdd8e2797c4ab3ff913ad9688d84
minkchoii <your_email@example.com>
2024-12-18 10:35:29 +0900

    Feat : store query service 페이징

--- src/main/java/spring/umc7th/service/storeService/StoreQueryService.java
+++ src/main/java/spring/umc7th/service/storeService/StoreQueryService.java
@@ -2,8 +2,10 @@ package spring.umc7th.service.storeService;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
 import spring.umc7th.domain.Member;
 import spring.umc7th.domain.Mission;
+import spring.umc7th.domain.Review;
 import spring.umc7th.domain.Store;
 import spring.umc7th.domain.mapping.MemberMission;
 
@@ -16,4 +18,6 @@ public interface StoreQueryService {
     Optional<Mission> findMission(Long id);
 
     Optional<MemberMission> findMemberMission(Member member, Mission mission);
+
+    Page<Review> getReviewList(Long storeId, Integer page);
 }
--- src/main/java/spring/umc7th/service/storeService/StoreQueryServiceImpl.java
+++ src/main/java/spring/umc7th/service/storeService/StoreQueryServiceImpl.java
@@ -3,14 +3,18 @@ package spring.umc7th.service.storeService;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.umc7th.domain.Member;
 import spring.umc7th.domain.Mission;
+import spring.umc7th.domain.Review;
 import spring.umc7th.domain.Store;
 import spring.umc7th.domain.mapping.MemberMission;
 import spring.umc7th.repository.MemberMissionRepository;
 import spring.umc7th.repository.MissionRepository;
+import spring.umc7th.repository.ReviewRepository;
 import spring.umc7th.repository.storeRepository.StoreRepository;
 
 @Service
@@ -21,6 +25,7 @@ public class StoreQueryServiceImpl implements StoreQueryService {
     private final StoreRepository storeRepository;
     private final MissionRepository missionRepository;
     private final MemberMissionRepository memberMissionRepository;
+    private final ReviewRepository reviewRepository;
 
     @Override
     public Optional<Store> findStore(Long id) {
@@ -44,5 +49,14 @@ public class StoreQueryServiceImpl implements StoreQueryService {
     @Override
     public Optional<MemberMission> findMemberMission(Member member, Mission mission) {
         return memberMissionRepository.findByMemberAndMission(member, mission);
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long storeId, Integer page) {
+
+        Store store = storeRepository.findById(storeId).get();
+
+        Page<Review> storePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return storePage;
     }
 }
